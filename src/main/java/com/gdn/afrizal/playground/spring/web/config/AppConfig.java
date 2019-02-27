c6631cecfb15343f0ff9f3533b7805712a5e0017
koneko096 <laser.survivor@gmail.com>
2019-02-27 12:06:56 +0700

    Decouple data config

--- src/main/java/com/gdn/afrizal/playground/spring/web/config/AppConfig.java
+++ src/main/java/com/gdn/afrizal/playground/spring/web/config/AppConfig.java
@@ -1,31 +1,36 @@
 package com.gdn.afrizal.playground.spring.web.config;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import org.springframework.context.annotation.Bean;
+import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import static java.util.Collections.singletonList;
-
 @Configuration
+@PropertySource("classpath:application.properties")
 @EnableWebMvc
 @EnableMongoRepositories(basePackages = "com.gdn.afrizal.playground.spring.data.repository")
 @ComponentScan(basePackages = "com.gdn.afrizal.playground.spring")
 public class AppConfig extends AbstractMongoConfiguration {
+
+  @Value("${spring.data.mongodb.database}")
+  private String databaseName;
+
+  @Value("${spring.data.mongodb.uri}")
+  private String uri;
+
+
   @Override
-  @Bean
   public MongoClient mongoClient() {
-    return new MongoClient(singletonList(new ServerAddress("127.0.0.1", 27017)),
-        singletonList(MongoCredential.createCredential("name", getDatabaseName(), "pwd".toCharArray())));
+    return new MongoClient(new MongoClientURI(uri));
   }
 
   @Override
-  public String getDatabaseName() {
-    return "spring-pg";
+  protected String getDatabaseName() {
+    return databaseName;
   }
 }
